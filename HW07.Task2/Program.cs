57288ec647dfceb08ee35e667d27c2a1784bbeac
Denis Samusevich <samynchik@gmail.com>
2019-07-15 01:47:09 +0300

    homework07 is done and added to GitHub

---
+++ HW07.Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW07.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] user_numbers = new int[10];
+            int[] copy_numbers = new int[10];
+            int index_number;
+            Console.WriteLine("input 9 random numbers");
+            for (int i = 0; i < user_numbers.Length-1; i++)
+                user_numbers[i] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < user_numbers.Length; i++)
+                Console.Write("\t"+user_numbers[i]);
+            Console.WriteLine("\ninput index from 0 to 9");
+            index_number = Convert.ToInt32(Console.ReadLine());
+            Array.Copy(user_numbers, 0, copy_numbers, 0 , index_number);
+            Array.Copy(user_numbers, index_number, copy_numbers, index_number + 1, 9 - index_number);
+            Console.WriteLine("input random number");
+            copy_numbers[index_number] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < user_numbers.Length; i++)
+            {
+                Console.Write("\t" + copy_numbers[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ HW07.Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW07.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first_element = 'A';
+            char last_element = 'Z';
+            char[] alphabet = new char[26];
+            char[] reverse_alphabet = new char[26];
+            for (int i = first_element; i <= last_element; i++)
+                alphabet[i - first_element] = (char)i;
+            Console.WriteLine(alphabet);
+            for (int i = 0, j= alphabet.Length-1; i < alphabet.Length; i++, j--)
+                reverse_alphabet[i] = alphabet[j];
+            for (int i = 0; i < alphabet.Length; i++)
+                alphabet[i] = reverse_alphabet[i];
+            Console.WriteLine(alphabet);
+            Array.Reverse(alphabet);
+            Console.WriteLine(alphabet);
+            Console.Read();
+        }
+    }
+}
