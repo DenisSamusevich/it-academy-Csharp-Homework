78d4656d572f1efde0d0314a9115a3715439f14c
Denis Samusevich <samynchik@gmail.com>
2019-07-21 15:04:09 +0300

    homework09 is done

---
+++ AssemblyOne/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class Employee
+    {
+        public const int NumberConstant = 10;
+        public string Name = String.Empty;
+        public bool Flag;
+        public int NumberPublic = 6;
+        protected int NumberProtected = 5;
+        internal int NumberInternal = 5;
+        private int NumberPrivate = 4;
+        protected internal int NumberProtectedInternal = 3;
+        private protected int NumberPrivateProtected = 2;
+
+
+        int Multiplication(int num)
+        {
+            num = NumberPublic;
+            num = NumberProtected;
+            num = NumberInternal;
+            num = NumberPrivate;
+            num = NumberProtectedInternal;
+            num = NumberPrivateProtected;
+            return num;
+        }
+    }
+}
---
+++ AssemblyOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Employee Employee = new Employee();
+            Develop Develop = new Develop();
+
+            num = Employee.NumberPublic;
+            num = Employee.NumberProtected; //this method is not available here as a Protected
+            num = Employee.NumberInternal; 
+            num = Employee.NumberPrivate; //this method is not available here as a Private
+            num = Employee.NumberProtectedInternal;
+            num = Employee.NumberPrivateProtected; //this method is not available here as a PrivateProtected
+
+        }
+    }
+}
