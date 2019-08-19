b1e3833325d6451466a01cce891b8bb6448bc1d7
Denis Samusevich <samynchik@gmail.com>
2019-08-19 22:20:15 +0300

    Database is created, log4net is customized, logfile is added.

---
+++ HW13.Task1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace HW13.Task1
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            string baseDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory));
+            GlobalContext.Properties["LogFileName"] = baseDirectory;
+            XmlConfigurator.Configure();
+            log.Info("Entering application.");
+            MotorcycleConsoleWrite.Greуting();
+            start:
+            MotorcycleConsoleWrite.WriteInputNumberMainMenu();
+            switch (MotorcycleConsoleInput.InputConsoleNumber(1, 5))
+            {
+                case 1:
+                    {
+                        MotorcycleConsoleWrite.WriteAllMotorcycles(MotorcycleRepository.GetMotorcycles());
+                        goto start;
+                    }
+                case 2:
+                    {
+                        MotorcycleConsoleWrite.WriteMotorcycle(MotorcycleRepository.GetMotorcycleByID(MotorcycleConsoleInput.InputConsoleNumberId(MotorcycleDataBase.MinNumberCollection, MotorcycleDataBase.СurrentNumberCollection)));
+                        goto start;
+                    }
+                case 3:
+                    {
+                        MotorcycleConsoleWrite.WriteCreateMotorcycle(out Motorcycle motorcycleCreate);
+                        MotorcycleRepository.CreateMotorcycle(motorcycleCreate);
+                        goto start;
+                    }
+                case 4:
+                    {
+                        MotorcycleConsoleWrite.WriteUpdateMotorcycle(MotorcycleRepository.GetMotorcycleByID(MotorcycleConsoleInput.InputConsoleNumberIdUpdate(MotorcycleDataBase.MinNumberCollection, MotorcycleDataBase.СurrentNumberCollection)), out Motorcycle motorcycleUpdate);
+                        MotorcycleRepository.UpdateMotorcycle(motorcycleUpdate);
+                        goto start;
+                    }
+                case 5:
+                    {
+                        if (MotorcycleConsoleWrite.WriteDeleteMotorcycle(MotorcycleRepository.GetMotorcycleByID(MotorcycleConsoleInput.InputConsoleNumberIdDelete(MotorcycleDataBase.MinNumberCollection, MotorcycleDataBase.СurrentNumberCollection)),out int inputId))
+                        {
+                            MotorcycleRepository.DeleteMotorcycle(inputId);
+                            goto start;
+                        }
+                        else
+                        {
+                            goto start;
+                        }
+                    }
+                default:
+                    goto start;
+            }
+            
+        }
+    }
+}
